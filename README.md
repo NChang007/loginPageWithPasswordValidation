4b094f02b7f6a5db4cc8b700ce7ac6b4252d3fb1
dchang74 <dchang74@gatech.edu>
2022-10-26 20:45:33 -0400

    fully functional with responsive visuals

---
+++ README.md
@@ -0,0 +1,8 @@
+LogIn Form with Password Validation 
+
+validation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+
+
+![LogIn Form Snip](./LoginForm.PNG "LogIn Form Picture")
+![LogIn FAIL Snip](./LoginFormRED.PNG "LogIn Form Picture")
+![LogIn PASS Snip](./LoginFormGREEN.PNG "LogIn Form Picture")
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 let pass = document.querySelector('#password')
-let loBtn = document.querySelector()
+let loBtn = document.querySelector('#submit')
+console.log(loBtn);
 pass.addEventListener('input', (e) => validate(e))
 
-let passIsGood = false
+// this velidates that the password has lower, capital, nums, special characters and 
 let passVal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+
 function validate(e) {
     console.log(e.target.value);
+
     let passValue = e.target.value
+
     if (passValue.match(passVal)) {
         pass.style.border = '2px solid green'
-        passIsGood = true
-    }else if(passValue = ""){
-        passIsGood = false
+        loginBtn()
     }else {
         pass.style.border = '2px solid red'
-        pass.style.focus.border = "2px solid red"
-        passIsGood= false
     }
 }
 
-const login = () => {
-    loBtn.classList.remove('deactivated')
+const loginBtn = () => {
+    loBtn.setAttribute('href', '');
+    console.log(loBtn);
 }
